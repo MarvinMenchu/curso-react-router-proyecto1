5c4ffc0354eda262edeb6021465cc006a8fb8c93
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2024-03-21 16:27:19 -0600

    APLICACION DE SLUG Y USE PARAMS

---
+++ src/blogdata.js
@@ -0,0 +1,21 @@
+const blogdata = []
+blogdata.push({
+    title: "Que es React?",
+    slug: "que-es-react",
+    content: "React es una libreria de JavaScript para construir interfaces de usuario",
+    author: "Marvin Menchu"
+})
+blogdata.push({
+    title: "Que es Vue?",
+    slug: "que-es-vue",
+    content: "Vue es una libreria de JavaScript para construir interfaces de usuario",
+    author: "Manuel Menchu"
+})
+blogdata.push({
+    title: "Que es Angular?",
+    slug: "que-es-angular",
+    content: "Angular es una libreria de JavaScript para construir interfaces de usuario",
+    author: "Quien? Menchu"
+})
+
+export { blogdata }
